8078734cd98114d57ed12de8328b5a639ffe5997
muharamrizal2 <muharamrizal2@gmail.com>
2022-09-02 17:08:49 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import ResponsiveAppBar from './components/NavBar'
+function App() {
+  return (
+    <>
+    <ResponsiveAppBar></ResponsiveAppBar>
+    </>
+  );
+}
+
+export default App;
