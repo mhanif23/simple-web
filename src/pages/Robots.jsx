40ecc71a110ef8dc680f9a0a762b42b9ee9be575
muharamrizal2 <muharamrizal2@gmail.com>
2022-09-02 18:53:49 +0700

    add use state example

--- src/pages/Robots.jsx
+++ src/pages/Robots.jsx
@@ -1,11 +1,32 @@
 import * as React from 'react';
+import { useState } from "react";
 import ResponsiveAppBar from '../components/NavBar'
 
 const Robots = () => {
+  const [robot, setRobots] = useState([])
+  const [newRobots, setNewRobots] = useState("")
+
+  const handleChange = (event) => {
+    setNewRobots(event.target.value)
+  } 
+
+  const updateRobots = () => {
+    const newRobot = [...robot, newRobots];
+    setRobots(newRobot)
+  }
   return (
     <>
       <ResponsiveAppBar></ResponsiveAppBar>
       <div> Hello from Robots</div>
+      <input onChange={handleChange}></input>
+      <button onClick={updateRobots}>add robots</button>
+
+      <div>List of your robots</div>
+      <div>
+        {robot.map((val) => { 
+          return <h3>{val}</h3>
+        })}
+      </div>
     </>
   );
 };
