fec68f71ee6604bd11ffabd5938f3263c88ff21f
muharamrizal2 <muharamrizal2@gmail.com>
2022-09-02 17:45:13 +0700

    add 2 pages to router dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Home from '../src/pages/home';
+import Home  from '../src/pages/home';
+import About  from '../src/pages/about';
+import Robots  from '../src/pages/robots';
 
 function App() {
   return( 
     <Router>
       <Routes>
-      <Route path="/" element= {<Home />}/>
+        <Route path="/" element= {<Home />}/>
+        <Route path="/about" element= {<About />}/>
+        <Route path="/robots" element= {<Robots />}/>
       </Routes>
     </Router>
   )
